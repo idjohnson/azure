896aead3200d13c178ef7e797a8884be66224e5d
James Bannan <james@bannan.com.au>
2015-07-07 10:11:33 +1000

    New-WindowsVM & Configure-WinRM

---
+++ Configure-WinRM.ps1
@@ -0,0 +1,34 @@
+﻿$serviceName = 'sccmlabcloud-01'
+$vmName = 'WINDEMO-01'
+$winRMCert = (Get-AzureVM -ServiceName $serviceName -Name $vmName | Select-Object -ExpandProperty vm).DefaultWinRMCertificateThumbprint
+ 
+$AzureX509cert = Get-AzureCertificate -ServiceName $serviceName -Thumbprint $winRMCert -ThumbprintAlgorithm sha1
+ 
+$certTempFile = [IO.Path]::GetTempFileName()
+$AzureX509cert.Data | Out-File $certTempFile
+ 
+$CertToImport = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $certTempFile
+ 
+$store = New-Object System.Security.Cryptography.X509Certificates.X509Store 'Root', 'LocalMachine'
+$store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
+$store.Add($CertToImport)
+$store.Close()
+ 
+Remove-Item $certTempFile
+
+
+#################
+
+
+$username = 'azureuser'
+$pwd = ConvertTo-SecureString –String 'P@ssw0rd' –AsPlainText -Force
+$credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $username, $pwd
+
+$uri = Get-AzureWinRMUri -ServiceName $serviceName -Name $VMName 
+
+Invoke-Command -ConnectionUri $uri -Credential $credential -ScriptBlock {
+    winrm quickconfig -q
+}
+
+#################
+
---
+++ New-WindowsVM.ps1
@@ -0,0 +1,11 @@
+﻿
+$winvm = New-AzureVMConfig `
+    -Name WINDEMO-01 `
+    -InstanceSize Basic_A1 `
+    -ImageName a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-R2-201505.01-en.us-127GB.vhd `
+
+$winvm | Add-AzureProvisioningConfig -Windows -AdminUsername 'azureuser' -Password 'P@ssw0rd' -EnableWinRMHttp
+
+$winvm | Set-AzureSubnet -SubnetNames 'Subnet-1'
+
+New-AzureVM -ServiceName sccmlabcloud-01 -VMs $winvm -VNetName 'Steamdriven-vNet001' -WaitForBoot -Verbose
