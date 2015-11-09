c295f85a702009c4815ab76aeaf776ade4adeb8b
James Bannan <james@bannan.com.au>
2015-11-09 14:03:29 -0600

    Updated Linux deployment scripts

--- resource-manager/Create-LinuxClientARM.ps1
+++ resource-manager/Create-LinuxClientARM.ps1
@@ -2,16 +2,33 @@
 $AzureLocation = 'West US' ### Use "Get-AzureLocation | Where-Object Name -eq 'ResourceGroup' | Format-Table Name, LocationsString -Wrap" in ARM mode to find locations which support Resource Groups
 $GroupName = 'chef-demo'
 $DeploymentName = 'linux-node-deployment'
+$templateBaseUrl = 'https://raw.githubusercontent.com/jamesbannan/azure/master/azure-rm/templates'
+$resourceTemplate = 'resourceVirtualMachine.json'
+$templateUrl = $templateBaseUrl + '/' + $resourceTemplate
+$password = Read-Host -AsSecureString
+$AzureResourceGroup = Get-AzureRmResourceGroup -Name $GroupName
 
-if((Test-AzureResourceGroup -ResourceGroupName $GroupName) -eq $false){
-    New-AzureResourceGroup -Name $GroupName -Location $Location -Verbose
-    $AzureResourceGroup = Get-AzureResourceGroup -Name $GroupName
-    }
-    else {$AzureResourceGroup = Get-AzureResourceGroup -Name $GroupName}
+### Get Chef organization validation key
+$ChefValidationCertificatePath = $env:USERPROFILE + '\Documents\Git\chef-demo\.chef'
+$ChefValidationCertificateName = 'chefdemo-validator.pem'
+$ChefValidationCertificate = $ChefValidationCertificatePath + '\' + $ChefValidationCertificateName
+$ChefValidationCertificateKey = Get-Content $ChefValidationCertificate -Raw
+
+### Get client.rb bootstrap settings
+$ClientRbPath = $env:USERPROFILE + '\Documents\Git\chef-demo\.chef'
+$ClientRb = $ClientRbPath + '\' + 'client.rb'
+$ClientRbProperties = Get-Content $ClientRb -Raw
+
+### Define additional template parameters
+$additionalParameters = New-Object -TypeName Hashtable
+$additionalParameters['adminPassword'] = $password
+$additionalParameters['validation_key'] = $ChefValidationCertificateKey
+$additionalParameters['client_rb'] = $ClientRbProperties
 
 New-AzureResourceGroupDeployment `
     -Name $DeploymentName `
     -ResourceGroupName $AzureResourceGroup.ResourceGroupName `
     -TemplateFile LinuxChefNode.json `
     -TemplateParameterFile LinuxChefNode.parameters.json `
-    -Verbose
+    @additionalParameters `
+    -Verbose -Force
--- resource-manager/Create-WindowsClientARM.ps1
+++ resource-manager/Create-WindowsClientARM.ps1
@@ -2,18 +2,25 @@
 $GroupName = 'chef-demo'
 $DeploymentName = 'windows-node-deployment'
 $AzureLocation = 'West US'
+$templateBaseUrl = 'https://raw.githubusercontent.com/jamesbannan/azure/master/azure-rm/templates'
+$resourceTemplate = 'resourceVirtualMachine.json'
+$templateUrl = $templateBaseUrl + '/' + $resourceTemplate
+$password = Read-Host -AsSecureString
+$AzureResourceGroup = Get-AzureRmResourceGroup -Name $GroupName
 
-if((Test-AzureRmResourceGroup -ResourceGroupName $GroupName) -eq $false){
-    New-AzureRmResourceGroup -Name $GroupName -Location $AzureLocation -Verbose
-    $AzureResourceGroup = Get-AzureRmResourceGroup -Name $GroupName
-    }
-    else {$AzureResourceGroup = Get-AzureRmResourceGroup -Name $GroupName}
+$ChefValidationCertificatePath = $env:USERPROFILE + '\Documents\Git\chef-demo\.chef'
+$ChefValidationCertificateName = 'chefdemo-validator.pem'
+$ChefValidationCertificate = $ChefValidationCertificatePath + '\' + $ChefValidationCertificateName
+$ChefValidationCertificateKey = Get-Content $ChefValidationCertificate -Raw
+
+$ClientRbPath = $env:USERPROFILE + '\Documents\Git\chef-demo\.chef'
+$ClientRb = $ClientRbPath + '\' + 'client.rb'
+$ClientRbProperties = Get-Content $ClientRb -Raw
 
 $additionalParameters = New-Object -TypeName Hashtable
-
-$password = Read-Host -AsSecureString
-
 $additionalParameters['adminPassword'] = $password
+$additionalParameters['validation_key'] = $ChefValidationCertificateKey
+$additionalParameters['client_rb'] = $ClientRbProperties
 
 New-AzureRmResourceGroupDeployment `
     -Name $DeploymentName `
