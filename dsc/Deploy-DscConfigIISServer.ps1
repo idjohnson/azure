1050f60661cac942d1c051f53c6c7e9ea668bd8d
James Bannan <james@bannan.com.au>
2016-04-13 11:30:11 +1000

    Created & uploaded Deploy-DscConfigIISServer

---
+++ dsc/Deploy-DscConfigIISServer.ps1
@@ -0,0 +1,35 @@
+﻿
+Configuration IISServer {
+
+    Node $env:COMPUTERNAME {
+
+
+        WindowsFeature WebServer {
+            Name = 'Web-Server'
+            Ensure = 'Present'
+        }
+
+        WindowsFeature WebAspNet45 {
+            Name = 'Web-Asp-Net45'
+            Ensure = 'Present'
+            DependsOn = '[WindowsFeature]WebServer'
+        }
+
+        }
+
+    }
+
+$ConfigurationData = @{
+    AllNodes = @(
+        @{
+            NodeName = "$env:COMPUTERNAME"
+        }
+    )
+}
+
+Write-Verbose -Message 'Compiling IIS DSC file.'
+IISServer -OutputPath "$env:windir\Temp\$env:COMPUTERNAME" -VMName $env:COMPUTERNAME -ConfigurationData $ConfigurationData
+Write-Verbose -Message 'Applying DSC config'
+Start-DscConfiguration -Wait -Verbose -Path "$env:windir\Temp\$env:COMPUTERNAME" -Force
+
+
